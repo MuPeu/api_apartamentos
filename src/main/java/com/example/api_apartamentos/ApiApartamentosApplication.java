cc893764f0a2bd5f06b22337a9958013c9da05cd
MuPeu <murilo.franca9@etec.sp.gov.br>
2025-10-28 16:31:27 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/example/api_apartamentos/ApiApartamentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_apartamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApartamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApartamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_apartamentos/ApiApartamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_apartamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApartamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
